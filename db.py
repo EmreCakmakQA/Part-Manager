92cfd14d161d31d6cf24713e5ff63b0e218b6459
EmreCakmakQA <ECakmak@qa.com>
2021-03-17 17:01:05 +0000

    Add database class methods, as well aspre-populated data

--- db.py
+++ db.py
@@ -8,3 +8,34 @@ class Database:
         self.cur.execute(
             "CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, part text, customer text, retailer text, price text)")
         self.conn.commit()
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM parts")  # Read all
+        rows = self.cur.fetchall()
+        return rows
+
+    def insert(self, part, customer, retailer, price):
+        self.cur.execute("INSERT INTO parts VALUES (NULL, ?, ?, ?, ?)",  # Protection against SQL injection
+                         (part, customer, retailer, price))
+        self.conn.commit()
+
+    def remove(self, id):
+        self.cur.execute("DELETE FROM parts WHERE id=?",
+                         (id,))  # Trailing comma for tuple
+        self.conn.commit()
+
+    def update(self, id, part, customer, retailer, price):
+        self.cur.execute("UPDATE parts SET part = ?, customer = ?, retailer = ?, price = ? WHERE id = ?",
+                         (part, customer, retailer, price, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
+
+db = Database('store.db')  # database name 'store.db'
+db.insert("8GB DDR4 RAM", "I.P Freely", "Argos", "130")
+db.insert("NVDIA 3080 GPU", "Maya Normousbutt", "Currys", "700")
+db.insert("INTEL I7-4790K CPU", "Ben Dover", "PC World", "250")
+db.insert("24 Inch 120Hz ASUS Monitor", "Oliver Klozoff", "eBay", "500")
+db.insert("Gaming Mouse ROG ASUS", "Mike Rotch", "Amazon", "85")
